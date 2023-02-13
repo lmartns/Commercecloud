2accf476dff32e4a29e16f6729bbca1efa13e0c6
leonardomartins578@gmail.com <leonardomartins578@gmail.com>
2023-02-13 11:56:58 -0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
   router: {
-    base: "/<repository-name>/",
+    base: "/Commercecloud/",
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
