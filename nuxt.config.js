10abc3380da0c512326f12c8f86b116f1d485622
leonardomartins578@gmail.com <leonardomartins578@gmail.com>
2023-02-13 11:28:32 -0300

    git page

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
+  router: {
+    base: "/<repository-name>/",
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
